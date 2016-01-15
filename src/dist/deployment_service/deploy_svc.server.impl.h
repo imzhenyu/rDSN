3345c3829e5fd413275158ab8ab48e9513126e3a
keyang <v-yank@microsoft.com>
2016-01-15 11:40:28 +0800

    allow deployment to use registered client commond

--- src/dist/deployment_service/deploy_svc.server.impl.h
+++ src/dist/deployment_service/deploy_svc.server.impl.h
@@ -1,8 +1,11 @@
 # pragma once
 
 # include "deploy_svc.server.h"
+# include "deploy_svc.types.h"
 # include <unordered_map>
 # include <dsn/dist/cluster_scheduler.h>
+
+
 
 namespace dsn 
 {
@@ -13,7 +16,12 @@ namespace dsn
             : public deploy_svc_service
         {
         public:
+            deploy_svc_service_impl();
+            ~deploy_svc_service_impl();
+
             error_code start();
+
+            void stop();
             
             virtual void on_deploy(const deploy_request& req, ::dsn::rpc_replier<deploy_info>& reply) override;
 
@@ -24,6 +32,16 @@ namespace dsn
             virtual void on_get_service_info(const std::string& service_url, ::dsn::rpc_replier<deploy_info>& reply) override;
 
             virtual void on_get_cluster_list(const std::string& format, ::dsn::rpc_replier<cluster_list>& reply) override;
+
+            void on_deploy_cli(void *context, int argc, const char **argv, dsn_cli_reply *reply);
+
+            void on_undeploy_cli(void *context, int argc, const char **argv, dsn_cli_reply *reply);
+
+            void on_get_service_list_cli(void *context, int argc, const char **argv, dsn_cli_reply *reply);
+
+            void on_get_service_info_cli(void *context, int argc, const char **argv, dsn_cli_reply *reply);
+
+            void on_get_cluster_list_cli(void *context, int argc, const char **argv, dsn_cli_reply *reply);
 
         private:
             mutable ::dsn::service::zrwlock_nr _service_lock;
@@ -39,6 +57,12 @@ namespace dsn
             std::unordered_map<std::string, std::shared_ptr<cluster_ex> > _clusters;
 
             std::string _service_dir;
+
+            dsn_handle_t _cli_deploy;
+            dsn_handle_t _cli_undeploy;
+            dsn_handle_t _cli_get_service_list;
+            dsn_handle_t _cli_get_service_info;
+            dsn_handle_t _cli_get_cluster_list;
 
         private:
             void download_service_resource_completed(error_code err, std::shared_ptr<::dsn::dist::deployment_unit> svc);
@@ -56,6 +80,17 @@ namespace dsn
                 ::dsn::error_code err,
                 const std::string& err_msg
                 );
+
+            void on_deploy_internal(const deploy_request& req, /*out*/ deploy_info& di);
+
+            void on_undeploy_internal(const std::string& service_name, /*out*/ error_code& err);
+            
+            void on_get_service_list_internal(const std::string& package_id, deploy_info_list& dlist);
+
+            void on_get_service_info_internal(const std::string& service_url, deploy_info& di);
+
+            void on_get_cluster_list_internal(const std::string& format, cluster_list& clist);
+
         };
     }
 }
